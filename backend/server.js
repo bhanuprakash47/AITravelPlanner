15620e1ad383eacb670d6d3722378cfc9f089e91
bhanuprakash47 <bhanuprakashnampally54@gmail.com>
2026-06-23 00:53:15 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use("/api/auth",router)
 app.use("/api/trips",tripRouter)
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 5000
 
 
 const initializeServerAndDB=async()=>{
